bf7820a90954c0c669ff3316ac123d88471d1e69
sosafacun <facundososa+github@slmail.me>
2024-06-26 09:39:32 -0300

    Minor bug fix
    The score now resets properly

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 signal start
 
+func _ready():
+	Globals.cpu_score = 0
+	Globals.player_score = 0
+
 func _process(_delta):
 	if(Input.is_action_pressed("start")):
 		start.emit()
